d39a9d0df7af154b029e55ef4ff08ca6dc54e5e1
gamapradiptawirawan <gamapradipta88@gmail.com>
2020-10-06 19:07:34 +0700

    Remove layer module

--- layer/__init__.py
+++
@@ -1,6 +0,0 @@
-from layer.Convolution import Convolution
-from layer.Dense import Dense
-from layer.Detector import Detector
-from layer.Flatten import Flatten
-from layer.Layer import Layer
-from layer.Pooling import Pooling
