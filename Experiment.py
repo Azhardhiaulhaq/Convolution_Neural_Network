1f84688fc124f30be6a8edb33f241637460b9b2b
aisyah_izzah <13517046@std.stei.itb.ac.id>
2020-10-08 23:30:23 +0700

    Update schema to function

--- Experiment.py
+++ Experiment.py
@@ -24,15 +24,6 @@ class Experiment:
     # TODO
     def predict(self, output):
         pass
-
-    def get_red_matrix(self, image):
-        return image[:,:,2]
-
-    def get_green_matrix(self, image):
-        return image[:,:,1]
-
-    def get_blue_matrix(self, image):
-        return image[:,:,0]
         
     def read_image(dataset_path):
 
@@ -54,6 +45,42 @@ class Experiment:
         images = np.array([(cv2.resize(cv2.cvtColor((cv2.imread(img, 1)), cv2.COLOR_BGR2RGB), (150,150))) for img in file_path])
 
         return images, class_label
+
+    def schema_split(model, list_images, list_labels):
+
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=0)
+        
+        for train_index, test_index in sss.split(list_images, list_labels):
+            training_data, testing_data = list_images[train_index], list_images[test_index]
+            training_label, testing_label = list_labels[train_index], list_labels[test_index]
+
+        model.fit(training_data, training_label, epochs=10, verbose=2) # TRAIN MODEL
+        pred_label = model.predict_classes(testing_data, batch_size=10) # PREDICT DATA
+            
+        accuracy = accuracy_score(testing_label, pred_label)
+        print("Accuracy : ", accuracy)
+
+        print("Confussion Matrix : ")
+        tn, fp, fn, tp = confusion_matrix(testing_label, pred_label).ravel()
+        print("| tp |", tp)
+        print("| fp |", fp)
+        print("| tn |", tn)
+        print("| fn |", fn)
+
+    def schema_cross_validation(model, k, list_images, list_labels):
+        
+        kf = KFold(n_splits=k)
+
+        for train_index, test_index in kf.split(input_data):
+            training_data, testing_data = list_images[train_index], list_images[test_index]
+            training_label, testing_label = list_labels[train_index], list_labels[test_index]
+           
+            model.fit(training_data, training_label, epochs=5, verbose=2) # TRAIN MODEL
+            pred_label = model.predict_classes(testing_data, batch_size=k) # PREDICT DATA
+
+            accuracy = accuracy_score(testing_label, pred_label)
+            print("Accuracy : ", accuracy)
+
     
     if __name__ == "__main__":
         list_images = []
@@ -75,16 +102,16 @@ class Experiment:
         for label in test_label:
             list_labels.append(label)
 
-        model = MyCNN()
-        model.add(Convolution(num_filter =  4, input_size = (150,150,3), filter_size = (3,3)))
-        model.add(Detector())
-        model.add(Pooling(filter_size=(2,2), stride_size=1, mode="max"))
-        model.add(Convolution(num_filter =  8,filter_size = (3,3)))
-        model.add(Detector())
-        model.add(Pooling(filter_size=(2,2), stride_size=1, mode="max"))
-        model.add(Flatten())
-        model.add(Dense(256,"relu"))
-        model.add(Dense(1,"sigmoid"))
+        # model = MyCNN()
+        # model.add(Convolution(num_filter =  4, input_size = (150,150,3), filter_size = (3,3)))
+        # model.add(Detector())
+        # model.add(Pooling(filter_size=(2,2), stride_size=1, mode="max"))
+        # model.add(Convolution(num_filter =  8,filter_size = (3,3)))
+        # model.add(Detector())
+        # model.add(Pooling(filter_size=(2,2), stride_size=1, mode="max"))
+        # model.add(Flatten())
+        # model.add(Dense(256,"relu"))
+        # model.add(Dense(1,"sigmoid"))
 
         # MODEL MACHINE LEARNING
         model = tf.keras.models.Sequential([
@@ -98,51 +125,8 @@ class Experiment:
             tf.keras.layers.Dense(512, activation='relu'), 
             tf.keras.layers.Dense(1, activation='sigmoid')  
         ])
+
         model.compile(optimizer=RMSprop(lr=0.0001), loss='binary_crossentropy', metrics = ['accuracy'])
 
         list_images = np.array(list_images)
         list_labels = np.array(list_labels)
-
-        # Experiment 1
-        # Skema split 90% train data, 10% test data
-        print("DATA SPLIT SCHEMA")
-        sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=0)
-        for train_index, test_index in sss.split(list_images, list_labels):
-            training_data, testing_data = list_images[train_index], list_images[test_index]
-            training_label, testing_label = list_labels[train_index], list_labels[test_index]
-
-        model.fit(training_data, training_label, epochs=10, verbose=2) # TRAIN MODEL
-        pred_label = model.predict_classes(testing_data, batch_size=10) # PREDICT DATA
-        # pred_label = []
-        # i = 0
-        # for data in testing_data:
-        #     print("data ke-", i)
-        #     predict = model.forward_prop(data)
-        #     pred_label.append(predict)
-        #     i = i + 1
-            
-        accuracy = accuracy_score(testing_label, pred_label)
-        print("Accuracy : ", accuracy)
-
-        print("Confussion Matrix : ")
-        tn, fp, fn, tp = confusion_matrix(testing_label, pred_label).ravel()
-        print("    | 1 | 0 |")
-        print("| 1 |",tp,"|",fp)
-        print("| 0 |",fn,"|",tn)
-
-        # Experiment 2  
-        # Skema 10-fold cross validation
-        # print("10-FOLD CROSS VALIDATION SCHEMA")
-        # kf = KFold(n_splits=10)
-        # for train_index, test_index in kf.split(list_images):
-        #     training_data, testing_data = list_images[train_index], list_images[test_index]
-        #     training_label, testing_label = list_labels[train_index], list_labels[test_index]
-           
-        #     model.fit(training_data, training_label, epochs=5, verbose=2) # TRAIN MODEL
-        #     pred_label = model.predict_classes(testing_data, batch_size=10) # PREDICT DATA
-
-        #     accuracy = accuracy_score(testing_label, pred_label)
-        #     print("Accuracy : ", accuracy)
-
-
-
